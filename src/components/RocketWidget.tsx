a73ffa10b7476e9860605fbdc6672886a0805b6e
MH4Helal <crazzaraft@gmail.com>
2024-03-07 03:18:37 +0000

    More responsiveness stuff

--- src/components/RocketWidget.tsx
+++ src/components/RocketWidget.tsx
@@ -23,10 +23,10 @@ const RocketWidget: React.FC<RocketWidgetProps> = ({ rocket }) => {
   const readMoreText = showFullDescription ? 'Read Less' : 'Read More';
 
   return (
-    <div className="rocket-widget flex flex-col sm:flex-row w-full justify-center items-center bg-white p-4 rounded-lg gap-4 shadow-lg">
-      <img className="w-full sm:w-[170px] lg:w-[312px] h-auto sm:h-[150px] lg:h-[200px] rounded-md" src={rocket.flickr_images[0]} alt={rocket.name} />
+    <div className="rocket-widget flex flex-col lg:flex-row w-full justify-center items-center bg-white p-4 rounded-lg gap-4 shadow-lg">
+      <img className="w-full lg:w-[170px] xl:w-[312px] h-auto lg:h-[150px] xl:h-[200px] rounded-md" src={rocket.flickr_images[0]} alt={rocket.name} />
       
-      <div className='flex flex-col sm:max-w-[300px] w-full'>
+      <div className='flex flex-col lg:max-w-[300px] w-full'>
         <h2 className='text-xl font-bold'>{rocket.name}</h2>
         <p className={`description ${descriptionClass}`} >{rocket.description}</p>
         <button className="text-blue-500" onClick={toggleDescription}>{readMoreText}</button>
